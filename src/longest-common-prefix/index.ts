69f7b85c7b87b3ab0d90e25da9dc6331ea1d4cdf
HesamSe <hesam.seyfollahi@gmail.com>
2025-01-04 21:35:23 +0330

    longest-common-prefix

---
+++ src/longest-common-prefix/index.ts
@@ -0,0 +1,13 @@
+function longestCommonPrefix(strs: string[]): string {
+    let prefix = "";
+    for(let i=0; i<strs[0].length; i++){
+        for(let j=1;j<strs.length; j++){
+            if(strs[j][i] !== strs[0][i])
+                return prefix;
+        }
+        prefix = strs[0].substring(0,i+1)
+    }
+    return prefix;
+}
+
+export default longestCommonPrefix;
